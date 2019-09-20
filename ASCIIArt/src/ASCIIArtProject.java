3f0ab999dc46a935ab959ebd063122451065789b
s-muralidharank <s-muralidharank@53S500042105.it.bsd405.org>
2019-09-20 08:08:36 -0700

    Turning in project 1

---
+++ ASCIIArt/src/ASCIIArtProject.java
@@ -0,0 +1,73 @@
+
+public class ASCIIArtProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		catEars();
+		topMiddle();
+		design1();
+		design2();
+		design3();
+		catEyes();
+		catEyes();
+		design3();
+		design2();
+		design1();
+		topMiddle();
+		catEnd();
+		System.out.println("\n\nThis is a designer cat with shady eyes.");
+	}
+	
+	public static void catEars() {
+		System.out.println("\t||\t\t\t\t        ||");
+		System.out.println("\t|||||\t\t\t\t     |||||");
+		System.out.println("\t||||||||\t\t\t  ||||||||");
+		System.out.println("\t|||||||||||\t\t       |||||||||||");
+		System.out.println("\t||||||||||||||||\t  ||||||||||||||||");
+		// System.out.println("\t||||||||||||||\t\t    ||||||||||||||");
+		// System.out.println("\t|||||||||||||||||\t |||||||||||||||||");	
+	}
+	
+	public static void topMiddle() {
+		System.out.println("\t||||||||||||||||||||!!||||||||||||||||||||");
+	}
+	
+	public static void design1() {
+		System.out.println("\t--------------------||-------------------");
+	}
+	
+	public static void design2() {
+		System.out.println("\t:):):):):):):):):):)||:):):):):):):):):):)"); 
+	}
+	
+	public static void design3() {
+		System.out.println("\t\"*\"*\"*\"*\"*\"*\"*\"*\"*\"*||*\"*\"*\"*\"*\"*\"*\"*\"*\"*\"");
+	}
+	
+	public static void catEyes() {
+		System.out.println("\t|    0O0O0O0O0O     ||     0O0O0O0O0O    |");
+	}	
+	
+	public static void catEnd() {
+		System.out.println("\t   |||||||||||||||||!!|||||||||||||||||");
+		System.out.println("\t      ||||||||||||||||||||||||||||||");
+		System.out.println("\t         ||||||||||||||||||||||||");
+	}
+	
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
